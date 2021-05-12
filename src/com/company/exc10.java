40ec2d70018d5acaf2a43b098455982559a99cb0
VinteKls_Dev <81206911+20Kg-3s@users.noreply.github.com>
2021-05-12 13:58:03 -0300

    Update exc10.java
    remoção de uma variável inútil

--- src/com/company/exc10.java
+++ src/com/company/exc10.java
@@ -6,7 +6,6 @@ public class exc10 {
     public static void main(String[] args){
         //Faça um programa que receba dois números inteiros
         // e gere os números inteiros que estão no intervalo compreendido por eles.
-        int loop  = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite dois números: ");
         int num1 = sc.nextInt();
