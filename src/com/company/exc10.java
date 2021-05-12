181c69a03d1066388cca4951f84a3594e17f2dd5
VinteKls_Dev <vintekls@hotmail.com>
2021-05-12 16:19:54 -0300

    Adicionado algoritimo caso o primeiro numero informado seja maior que o primeiro, mostre os numeros no intervalo entre eles.

--- src/com/company/exc10.java
+++ src/com/company/exc10.java
@@ -6,16 +6,19 @@ public class exc10 {
     public static void main(String[] args){
         //Faça um programa que receba dois números inteiros
         // e gere os números inteiros que estão no intervalo compreendido por eles.
-        int loop  = 0;
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite dois números: ");
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
 
-        do {
+        while (num1<num2){
             num1 ++;
             System.out.print(num1 + " ");
         }
-        while (num1<num2);
+
+        while (num2<num1){
+            num1--;
+            System.out.print(num1 + " ");
+        }
     }
 }
